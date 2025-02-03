9cb9df839204d6a568bdaed117fec74d4f42440f
ogola5 <ogolaevance5@gmail.com>
2025-02-03 21:59:45 +0300

    Initial commit.

---
+++ src/logistics_system_frontend/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from 'react';
+import { logistics_system_backend } from 'declarations/logistics_system_backend';
+
+function App() {
+  const [greeting, setGreeting] = useState('');
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const name = event.target.elements.name.value;
+    logistics_system_backend.greet(name).then((greeting) => {
+      setGreeting(greeting);
+    });
+    return false;
+  }
+
+  return (
+    <main>
+      <img src="/logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form action="#" onSubmit={handleSubmit}>
+        <label htmlFor="name">Enter your name: &nbsp;</label>
+        <input id="name" alt="Name" type="text" />
+        <button type="submit">Click Me!</button>
+      </form>
+      <section id="greeting">{greeting}</section>
+    </main>
+  );
+}
+
+export default App;
